3b32a9a6deb35bc1adce2aa76c12b4977cfa3298
James Le-Goff <jameslegoff92@gmail.com>
2023-05-17 11:51:36 -0400

    Fixed contant form size, fixed the text area resize issue

--- client/public/modules/animations/gsap.js
+++ client/public/modules/animations/gsap.js
@@ -1,3 +1,5 @@
 export const navAnimation = () => {
-  return gsap.from([".logo", ".c-nav__item"], {duration: 1, scale: 0, ease: "back", stagger: 0.1});
+  return gsap.from([".c-logo", ".c-nav__item"], {duration: 1, scale: 0, ease: "back", stagger: 0.1});
 };
+
+
